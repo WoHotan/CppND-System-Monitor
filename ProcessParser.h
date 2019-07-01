bd9a5dcf07122e1dec7ba437faade3a128c6106c
richard <richard_wen@wistron.com>
2019-07-01 01:44:57 -0400

    getVmsize

--- ProcessParser.h
+++ ProcessParser.h
@@ -72,3 +72,27 @@ vector<string> ProcessParser::getPidList() {
         throw std::runtime_error(std::strerror(errno));
     return container;
 }
+
+string getVmSize(string pid) {
+    string line;
+    //Declaring search attribute for file
+    string name = "VmData";
+    string value;
+    float result;
+    // Opening stream for specific file
+    ifstream stream = Util::getStream((Path::basePath() + pid + Path::statusPath()));
+    while(std::getline(stream, line)) {
+        //Searching line by line
+        if(line.compare(0, name.size(),name) == 0)) {
+            // slicing string line on ws for values using sstream
+            istringstream buf(line);
+            istream_iterator<string> beg(buf), end;
+            vector<string> values(beg, end);
+            //conversion kB-> GB
+            result = (stof(vlaues[1]) / float(1024 * 1024))
+            break;
+        }
+    }
+    return to_string(result);
+}
+
